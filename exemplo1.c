f3017b7da2812532ccb5235285b930a49dad7d0a
solocr0w <105529322+solocr0w@users.noreply.github.com>
2024-02-21 21:37:11 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,15 @@
+/*
+ *Lab 1.
+ *Exemplo 1.
+*/
+
+#include <stdio.h>>
+
+int main(){
+//imprime mensagens na tela
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e\n");
+  printf("Prog. II\n");
+  printf("Vamos estudar a linguagem C");
+  return 0;  
+}
