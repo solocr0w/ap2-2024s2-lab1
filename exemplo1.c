8b7846d5043f04b6f3af8ed951a5e448d8deb9b7
solocr0w <105529322+solocr0w@users.noreply.github.com>
2024-02-22 00:55:02 +0000

    Inclusão ano fundação

--- exemplo1.c
+++ exemplo1.c
@@ -8,6 +8,7 @@
 int main(){
 //imprime mensagens na tela
   printf("Mackenzie - FCI\n");
+  printf("Desde de 1970\n");
   printf("Ciência da Computação\n");
   printf("Algoritmos e Programação II\n");
   return 0;  
